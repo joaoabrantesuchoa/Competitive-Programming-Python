2d3cbcc7fc811c44b76639de40aaab5ee91d7ad8
joao-abrantes-uchoa <58981771+joaoabrantesuchoa@users.noreply.github.com>
2020-05-26 10:29:48 -0300

    Update floyd_warshall.py

--- floyd_warshall.py
+++ floyd_warshall.py
@@ -2,22 +2,57 @@ INF = 1e10
 
 n, m = map(int,input().split())
 
+distancia = [[INF]*n for i in range(n)]
+'''
+grafo = []
+for i in range(n):
+    lista = []
+    for j in range(n):
+        if i == j:
+            lista.append(0)
+        else:
+            lista.append(INF)
+    grafo.append(lista)
+    
+for x in range(m):
+    no1,no2,peso = map(int,input().split())
+    if peso < grafo[no1][no2]:
+        grafo[no1][no2] = peso
 
-d = [[INF]*n for i in range(n)]
+'''
 
 for x in range(m):
-    a,b,w = map(int,input().split())
+    no1,no2,peso = map(int,input().split())
 
-    d[a-1][b-1] = w
-    d[b-1][a-1] = w
-    d[a-1][a-1] = 0
-    d[b-1][b-1] = 0
+    d[no1-1][no2-1] = peso
+    d[no2-1][no1-1] = peso
+    d[no1-1][no1-1] = 0
+    d[no2-1][no2-1] = 0
 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+def floyd(): #Apenas para pesos positivos
+    for fase in range(n):
+        for saida in range(n):
+            for chegada in range(n):
+                distancia[saida][chegada] = min(distancia[saida][chegada], distancia[saida][fase] + distancia[fase][chegada])
 
-print(d)
+def floyd_negativo(): #pesos positivos e negativos
+    for fase in range(n):
+        for saida in range(n):
+            for chegada in range(n):
+                if distancia[saida][fase] < INF and distancia[fase][chegada] < INF:
+                    distancia[saida][chegada] = min(distancia[saida][chegada], distancia[saida][fase] + distancia[fase][chegada])
+                
+                
+def encontrar_ciclo(): #Basicamente você verifica se a distância do nó para si mesmo é menor do que 0, se for tem ciclo
+    for fase in range(n):
+        for saida in range(n):
+            chegada = 0
+            while distancia[fase][saida] != -INF and chegada < n: #Esses distancia != INF é só para verificar se é possível alcançar o nó, senão o algoritmo pode ir atrás de caminho que não existe.
+                if distancia[chegada][chegada] < 0 and distancia[fase][chegada] != INF and distancia[chegada][saida] != INF:
+                    distancia[fase][chegada] = -INF  #Coloca-se -INF para implicar uma menor distância infinita 
+                chegada += 1
 
-
+def verificar_presenca_ciclo(): #Só olha se tem ciclo negativo ou não, não coloca a distancia dele ou outra coisa, só olha
+    for no in range(n):
+        if distancia[no][no] < 0:
+            return True
